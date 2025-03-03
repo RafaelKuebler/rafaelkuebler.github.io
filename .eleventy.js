798c8c2f59f892292b4b0f06fd728630da6257dd
Rafael Kübler da Silva <rafael_kuebler@yahoo.es>
2025-03-03 11:16:36 +0100

    Add RSS feed and icon, upgrade dependencies

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,27 @@
 const { DateTime } = require("luxon");
 const markdownIt = require("markdown-it");
+const { feedPlugin } = require("@11ty/eleventy-plugin-rss");
 
 module.exports = function(eleventyConfig) {
+    eleventyConfig.addPlugin(feedPlugin, {
+		type: "rss",
+		outputPath: "/feed.xml",
+		collection: {
+			name: "post",
+			limit: 10,
+		},
+		metadata: {
+			language: "en",
+			title: "Rafa's blog",
+			subtitle: "Rafael's reflections on life and other things.",
+			base: "https://rafaelkuebler.github.io/",
+			author: {
+				name: "Rafael Kübler da Silva",
+				email: "rafael_kuebler@yahoo.es",
+			}
+		}
+	});
+
     eleventyConfig.addPassthroughCopy("css");
     eleventyConfig.addPassthroughCopy("media");
 
